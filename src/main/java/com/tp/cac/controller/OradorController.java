f10ced62fc779d27d6cabd319ddb8d0744b7e64f
Nicodc96 Desktop <diaznicox96@gmail.com>
2023-12-14 16:31:44 -0300

    update wrong mapping on oradorController

--- src/main/java/com/tp/cac/controller/OradorController.java
+++ src/main/java/com/tp/cac/controller/OradorController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 
 @RestController
-@RequestMapping("/api/v1/")
+@RequestMapping("/api/v1")
 @CrossOrigin(origins = "*")
 public class OradorController {
     @Autowired
